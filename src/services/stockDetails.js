c8fd1bb36203fb70382cb5c66e08efdfe726b24a
Prathmesh Bhoir <prbhoir2001@gmail.com>
2023-01-04 00:01:24 +0530

    .env changes (doesn't work)

--- src/services/stockDetails.js
+++ src/services/stockDetails.js
@@ -1,9 +1,13 @@
 import axios from "axios"
 
-// const KEY = process.env.api
+// const BASE_URL = process.env.BASE_URL_API
+// const token = process.env.API_TOKEN
+
+const token = "bv1uf4v48v6o5ed6h88g"
+const BASE_URL = "https://finnhub.io/api/v1/"
 
 export const getStock = async (stock) => {
-    const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=${stock}&token=bv1uf4v48v6o5ed6h88g`)
+    const response = await axios.get(`${BASE_URL}quote?symbol=${stock}&token=${token}`);
 
     return response.data;
 }
